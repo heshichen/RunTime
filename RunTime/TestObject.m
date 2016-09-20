1b1104b84906c8555e046e84d502454300e8170f
heshichen <heshichenjm@sina.com>
2016-09-20 18:05:33 +0800

    new file
    new a objecct

---
+++ RunTime/TestObject.m
@@ -0,0 +1,13 @@
+//
+//  TestObject.m
+//  RunTime
+//
+//  Created by heshichen on 16/9/20.
+//  Copyright © 2016年 heshichen. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+@end
